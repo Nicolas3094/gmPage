29df50e39bcd5e8b8b8448ab9f83fa75f4c35480
Nicolas3094 <nicoo.garcia94@gmail.com>
2025-03-06 13:23:28 -0600

    feat: Change basehref

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PrivacyComponent } from './components/privacy/privacy.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/index', pathMatch: 'full' },
+    { path: '**', redirectTo: "index" },
     { path: 'index', component: IndiceComponent },
     { path: 'privacy', component: PrivacyComponent },
     { path: 'images', loadComponent: () => import('./components/imagenes/imagenes.component').then(mod => mod.ImagenesComponent) }

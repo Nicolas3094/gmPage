01226f2905b9ddbc4b2f6c564da6fd4324710adb
Nicolas3094 <nicoo.garcia94@gmail.com>
2025-03-06 10:14:56 -0600

    feat: Update

--- src/app/components/indice/indice.component.ts
+++ src/app/components/indice/indice.component.ts
@@ -2,39 +2,31 @@ import { Component } from '@angular/core';
 import { IndexElement } from '../../models/index-element.model';
 import { MAIN_INDEX_LIST } from '../../app.config';
 import { NgFor, NgIf } from '@angular/common';
-import { DomSanitizer } from '@angular/platform-browser';
-import { SafeUrlPipe } from '../../safe-url.pipe';
 import { OverlayVideoComponent } from '../overlay-video/overlay-video.component';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-indice',
   imports: [NgFor, NgIf, OverlayVideoComponent],
   templateUrl: './indice.component.html'
 })
 export class IndiceComponent {
-  popup:boolean = false;
-  elementsList:Array<IndexElement> = MAIN_INDEX_LIST;
+  popup: boolean = false;
+  elementsList: Array<IndexElement> = MAIN_INDEX_LIST;
 
   currentIndexElement?: IndexElement;
-  sanitizer: DomSanitizer;
 
-  constructor(sanitizer: DomSanitizer) {
-    this.sanitizer = sanitizer;
-  }
-
-  handleButtoIndex(indexElement:IndexElement){
+  handleButtoIndex(indexElement: IndexElement) {
     console.log(indexElement.index);
     this.currentIndexElement = indexElement;
-    if (indexElement.type === "vimeo" || indexElement.type === "youtube") {
-      this.popup = true;
-    } else {
-      window.open(indexElement.url, "_blanks");
+    switch (indexElement.type) {
+      case "vimeo":
+      case "youtube":
+        this.popup = true;
+        break;
+      default:
+        window.open(indexElement.url, "_blanks");
     }
-  }
-
-  hanleUrl(url: string) {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   close() {

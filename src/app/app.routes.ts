53d113398bf31360b970eeba8eba4182cde77c46
Nicolas3094 <nicoo.garcia94@gmail.com>
2025-03-06 10:25:35 -0600

    feat: Update [2]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { IndiceComponent } from './components/indice/indice.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/index', pathMatch: 'full' },
     { path: 'index', component: IndiceComponent },
-    { path: 'privacy', loadComponent: () => import('./components/privacy/privacy.component').then(mod => mod.PrivacyComponent) },
+    { path: 'privacy', component: PrivacyComponent },
     { path: 'images', loadComponent: () => import('./components/imagenes/imagenes.component').then(mod => mod.ImagenesComponent) }
 ];
